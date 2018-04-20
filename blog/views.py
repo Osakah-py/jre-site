d8daa046df88430e202e15a4d6cac7466d842b41
alex <lescot.alexg@gmail.com>
2018-04-20 15:42:39 +0200

    ajout formulaire édition

--- blog/views.py
+++ blog/views.py
@@ -2,33 +2,10 @@ from django.shortcuts import render, get_object_or_404, render_to_response,redir
 from django.utils import timezone
 from .models import Post
 from django.contrib.auth import authenticate, login, logout
-from django.http import *
 from django.views.generic import TemplateView
 from django.conf import settings
+from .forms import PostForm
 
-class LoginView(TemplateView):
-
-  template_name = 'blog/post_list.html'
-
-  def post(self, request, **kwargs):
-
-    username = request.POST.get('username', False)
-    password = request.POST.get('password', False)
-    user = authenticate(username=username, password=password)
-    if user is not None and user.is_active:
-        login(request, user)
-        return HttpResponseRedirect( settings.LOGIN_REDIRECT_URL )
-
-    return render(request, self.template_name)
-
-class LogoutView(TemplateView):
-
-  template_name = 'blog/post_list.html'
-
-  def get(self, request, **kwargs):
-    logout(request)
-    return render(request, self.template_name)
-	
 def post_list(request):
     titre="JRE - Collège de l'aigle"
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -40,3 +17,36 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post, 'title':titre})	
     print (titre+"a été ouvert dans le navigateur")
 
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def page_not_found_view(request):
+     return render(request,'blog/404.html')
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+	
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
