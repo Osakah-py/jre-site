39f1ac5caedb6cbaae1b38a121cd1c76a41f8fb3
alex <lescot.alexg@gmail.com>
2018-04-20 22:49:40 +0200

    ajout

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+   list_display   = ('title', 'auteur', 'published_date')
+   search_fields  = ('title', 'text')
+   
 admin.site.site_header = 'JRE - Administration'
+admin.site.register(Post, PostAdmin)
 
-admin.site.register(Post)
--- mysite/settings.py
+++ mysite/settings.py
@@ -102,13 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_REDIRECT_URL = '/backoffice/'
 LOGIN_URL = '/'
+
 TEMPLATE_DIRS = (
     BASE_DIR + '/templates/',
-	'blog/templates',
 )
-TEMPLATE_LOADERS = [
-     'django.template.loaders.filesystem.loader'
-]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
