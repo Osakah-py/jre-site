98d9db6f28bdf6d7169421d8fa5c5d36d95d357d
alex <lescot.alexg@gmail.com>
2018-04-21 18:18:35 +0200

    ajout du modèle categorie avec les fonctions avancées d'administration

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Categorie
 
 class PostAdmin(admin.ModelAdmin):
    list_display   = ('title', 'auteur', 'published_date')
    search_fields  = ('title', 'text')
+
+class CategorieAdmin(admin.ModelAdmin):
+   list_display   = ('nom',)
+   search_fields  = ('nom',)
    
 admin.site.site_header = 'JRE - Administration'
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Categorie, CategorieAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     auteur = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="titre")
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, verbose_name="Date de parution")
+    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +17,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length=30)
+
+    def __str__(self):
+         return self.nom
