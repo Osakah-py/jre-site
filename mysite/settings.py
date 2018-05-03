6df74586153a45d3c77b8752b9806a79781cd13c
ALEX <38435159+Aiglons@users.noreply.github.com>
2018-05-03 21:31:29 +0200

    correction CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,4 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/image/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
 ALLOWED_HOSTS = ['127.0.0.1', 'jreaigle.pythonanywhere.com']
