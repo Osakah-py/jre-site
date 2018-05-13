e5f5750898e06846c6b9fadacf9f7c2ad18ed6f4
ALEX <38435159+Aiglons@users.noreply.github.com>
2018-05-13 21:28:19 +0200

    V.1.2.3:Correction beug lors de creation article
    Retrait du field image pour beugs et ajout des catégories dans le formulaire et correction du "NOT NULL constraint failed: blog_post.auteur_id"

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.auteur = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -43,7 +43,7 @@ def post_edit(request, pk):
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.auteur = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
